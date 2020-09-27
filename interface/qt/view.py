e103ea947760b1419778c58877a905f37e3814c6
Rob Applin <robert.applin@stfc.ac.uk>
2020-09-27 15:51:05 +0100

    View should inherit from ui object.

--- interface/qt/view.py
+++ interface/qt/view.py
@@ -4,25 +4,25 @@ from PyQt5 import QtCore
 
 from plotting.interactive_plot import InteractivePlot
 from qt.add_body_dialog import AddBodyDialog
+from qt.ui.mainwindow_ui import Ui_MainWindow
 
 
-class NBodySimulationsView(QtCore.QObject):
+class NBodySimulationsView(Ui_MainWindow, QtCore.QObject):
     removeBodySignal = QtCore.pyqtSignal()
     addBodySignal = QtCore.pyqtSignal()
     playPauseSignal = QtCore.pyqtSignal()
 
-    def __init__(self, ui_form):
+    def __init__(self, parent=None):
         super(NBodySimulationsView, self).__init__()
-
-        self.ui = ui_form
+        self.setupUi(parent)
 
         self.interactive_plot = InteractivePlot()
 
-        self.ui.plotLayout.addWidget(self.interactive_plot.canvas())
+        self.plotLayout.addWidget(self.interactive_plot.canvas())
 
-        self.ui.pbRemoveBody.clicked.connect(self.emit_remove_body_clicked)
-        self.ui.pbAddBody.clicked.connect(self.emit_add_body_clicked)
-        self.ui.pbPlayPause.clicked.connect(self.emit_play_pause_clicked)
+        self.pbRemoveBody.clicked.connect(self.emit_remove_body_clicked)
+        self.pbAddBody.clicked.connect(self.emit_add_body_clicked)
+        self.pbPlayPause.clicked.connect(self.emit_play_pause_clicked)
 
     def emit_remove_body_clicked(self) -> None:
         self.removeBodySignal.emit()
@@ -35,37 +35,37 @@ class NBodySimulationsView(QtCore.QObject):
 
     def clear(self) -> None:
         self.interactive_plot.clear()
-        self.ui.cbBodyNames.clear()
+        self.cbBodyNames.clear()
 
     def reset_view(self, bodies: dict) -> None:
         self.clear()
         self.add_bodies(bodies)
 
     def remove_body(self, body_name: str) -> None:
-        self.ui.cbBodyNames.removeItem(self.ui.cbBodyNames.currentIndex())
+        self.cbBodyNames.removeItem(self.cbBodyNames.currentIndex())
         self.interactive_plot.remove_body(body_name)
 
     def add_bodies(self, body_parameters: dict) -> None:
         for body_name, parameters in body_parameters.items():
-            self.ui.cbBodyNames.addItem(body_name)
+            self.cbBodyNames.addItem(body_name)
             self.interactive_plot.draw_body(body_name, parameters[0], parameters[1])
 
-        self.ui.cbBodyNames.setCurrentIndex(0)
+        self.cbBodyNames.setCurrentIndex(0)
 
     def add_body(self, body_name: str, position: tuple) -> None:
-        self.ui.cbBodyNames.addItem(body_name)
-        self.ui.cbBodyNames.setCurrentIndex(self.ui.cbBodyNames.count() - 1)
+        self.cbBodyNames.addItem(body_name)
+        self.cbBodyNames.setCurrentIndex(self.cbBodyNames.count() - 1)
 
         self.interactive_plot.draw_body(body_name, position[0], position[1])
 
     def selected_body(self) -> str:
-        return self.ui.cbBodyNames.currentText()
+        return self.cbBodyNames.currentText()
 
     def set_as_simulating(self, simulating: bool) -> None:
-        self.ui.pbPlayPause.setText("Pause" if simulating else "Play")
+        self.pbPlayPause.setText("Pause" if simulating else "Play")
 
     def is_simulating(self) -> bool:
-        return self.ui.pbPlayPause.text() != "Play"
+        return self.pbPlayPause.text() != "Play"
 
     @staticmethod
     def open_add_body_dialog() -> str:
