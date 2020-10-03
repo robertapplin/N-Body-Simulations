83ff0e4a1c7d5e369e3da2ff7883acb06b2211da
Robert Applin <robert.applin@stfc.ac.uk>
2020-10-03 11:08:34 +0100

    Add tests for the error catcher.

--- python/qt/error_catcher.py
+++ python/qt/error_catcher.py
@@ -2,22 +2,34 @@
 # Authored by Robert Applin, 2020
 from PyQt5 import QtWidgets
 
+from functools import wraps
+
 
 class ErrorReporter(QtWidgets.QMessageBox):
 
-    def __init__(self):
+    def __init__(self, message=""):
         super(ErrorReporter, self).__init__()
 
         self.setWindowTitle("Warning!")
         self.setIcon(QtWidgets.QMessageBox.Warning)
         self.setStandardButtons(QtWidgets.QMessageBox.Ok)
 
-        self._error_message = ""
+        self._error_message = message
 
     def __del__(self):
         if self._error_message:
             self.setText(self._error_message)
             self.exec_()
 
-    def report_error(self, message: str) -> None:
-        self._error_message = message
+
+def catch_errors(silent=False):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(self, *args, **kwargs):
+            try:
+                function(self, *args, **kwargs)
+            except Exception as ex:
+                if not silent:
+                    ErrorReporter(str(ex))
+        return wrapper
+    return decorator
---
+++ tests/unit_tests/python/qt/test_error_catcher.py
@@ -0,0 +1,56 @@
+# Project Repository : https://github.com/robertapplin/N-Body-Simulations
+# Authored by Robert Applin, 2020
+import pytest
+import sys
+from directory_helper import PYTHON_QT_DIRECTORY
+
+# Required to find python modules in parent directories
+sys.path.append(PYTHON_QT_DIRECTORY)
+
+from error_catcher import catch_errors
+
+
+class ErrorCauser:
+    def __init__(self):
+        pass
+
+    def cause_an_uncaught_exception(self):
+        raise RuntimeError("This is a RuntimeError.")
+
+    @catch_errors(silent=True)
+    def cause_an_exception(self):
+        raise RuntimeError("This is a RuntimeError.")
+
+    @catch_errors(silent=True)
+    def divide_by_zero(self):
+        return 10 / 0
+
+    @catch_errors(silent=True)
+    def index_out_of_range(self):
+        test_list = [0, 1, 2, 3]
+        return test_list[4]
+
+
+@pytest.fixture(scope='module')
+def error_causer():
+    return ErrorCauser()
+
+
+def test_that_the_error_causer_causes_an_error_when_not_using_the_error_catcher(error_causer):
+    try:
+        error_causer.cause_an_uncaught_exception()
+    except RuntimeError:
+        return
+    pytest.fail("The ErrorCauser class did not cause an exception when expected.")
+
+
+def test_that_an_exception_is_caught_by_the_error_catcher(error_causer):
+    error_causer.cause_an_exception()
+
+
+def test_that_a_divide_by_zero_error_is_caught_by_the_error_catcher(error_causer):
+    error_causer.divide_by_zero()
+
+
+def test_that_an_index_out_of_range_error_is_caught_by_the_error_catcher(error_causer):
+    error_causer.index_out_of_range()
