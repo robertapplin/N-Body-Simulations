019eef378b0461c27c2f8f35029a5835d4874375
Robert Applin <robert.applin@stfc.ac.uk>
2020-10-11 15:41:03 +0100

    Set up unit test environment.

---
+++ tests/unit_tests/cpp/main.cpp
@@ -0,0 +1,8 @@
+// Project Repository : https://github.com/robertapplin/N-Body-Simulations
+// Authored by Robert Applin, 2020
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
