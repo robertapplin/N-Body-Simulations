80339501b90ee74dfaf4e483bc4dc15e4f4e74ce
Robert Applin <robert.applin@stfc.ac.uk>
2020-10-25 14:57:36 +0000

    Tidy up changes to view.

--- python/qt/view.py
+++ python/qt/view.py
@@ -1,7 +1,6 @@
 # Project Repository : https://github.com/robertapplin/N-Body-Simulations
 # Authored by Robert Applin, 2020
 from n_body_simulations.add_body_dialog import AddBodyDialog
-from n_body_simulations.error_catcher import catch_errors
 from n_body_simulations.interactive_plot import InteractivePlot
 from n_body_simulations.main_window_ui import Ui_MainWindow
 from n_body_simulations.signal_blocker import SignalBlocker
@@ -40,8 +39,9 @@ class NBodySimulationsView(Ui_MainWindow, QObject):
         self.dsbDuration.valueChanged.connect(lambda value: self.emit_duration_changed(value))
 
     def handle_body_data_changed(self, row_index: int, column_index: int) -> None:
-        body_name = self.body_name_from_index(row_index)
-        new_value = self.get_value(row_index, column_index)
+        body_name = self._index_of_body(row_index)
+        new_value = self._get_table_value(row_index, column_index)
+
         if column_index == 1:
             self.massChangedSignal.emit(body_name, new_value)
         elif column_index == 2:
@@ -52,12 +52,8 @@ class NBodySimulationsView(Ui_MainWindow, QObject):
             self.xVelocityChangedSignal.emit(body_name, new_value)
         elif column_index == 5:
             self.yVelocityChangedSignal.emit(body_name, new_value)
-
-    def body_name_from_index(self, row_index: int) -> str:
-        return self.twBodyData.item(row_index, 0).text()
-
-    def get_value(self, row_index: int, column_index: int) -> float:
-        return float(self.twBodyData.item(row_index, column_index).text())
+        else:
+            raise RuntimeError("An unexpected column index was detected.")
 
     def emit_remove_body_clicked(self) -> None:
         self.removeBodyClickedSignal.emit()
@@ -92,11 +88,18 @@ class NBodySimulationsView(Ui_MainWindow, QObject):
         self.set_time_step(time_step)
         self.set_duration(duration)
 
+    def selected_body(self) -> str:
+        selected_index = self._selected_row_index()
+        if selected_index != -1:
+            return self._index_of_body(selected_index)
+        return None
+
     def remove_body(self, body_name: str) -> None:
         self.set_as_editing(True)
         self.handle_edit_clicked()
 
-        self.twBodyData.removeRow(self.selected_row_index())
+        self.twBodyData.removeRow(self._selected_row_index())
+
         self.interactive_plot.remove_body(body_name)
         self.interactive_plot.show_legend()
         self.interactive_plot.draw()
@@ -129,11 +132,11 @@ class NBodySimulationsView(Ui_MainWindow, QObject):
 
         self.twBodyData.insertRow(row_index)
         self.twBodyData.setItem(row_index, 0, QTableWidgetItem(body_name))
-        self.twBodyData.setItem(row_index, 1, self.create_table_float_item(body_data[0]))
-        self.twBodyData.setItem(row_index, 2, self.create_table_float_item(body_data[1].x))
-        self.twBodyData.setItem(row_index, 3, self.create_table_float_item(body_data[1].y))
-        self.twBodyData.setItem(row_index, 4, self.create_table_float_item(body_data[2].x))
-        self.twBodyData.setItem(row_index, 5, self.create_table_float_item(body_data[2].y))
+        self.twBodyData.setItem(row_index, 1, self._create_table_value(body_data[0]))
+        self.twBodyData.setItem(row_index, 2, self._create_table_value(body_data[1].x))
+        self.twBodyData.setItem(row_index, 3, self._create_table_value(body_data[1].y))
+        self.twBodyData.setItem(row_index, 4, self._create_table_value(body_data[2].x))
+        self.twBodyData.setItem(row_index, 5, self._create_table_value(body_data[2].y))
 
     def set_time_step(self, time_step: float) -> None:
         _ = SignalBlocker(self.dsbTimeStep)
@@ -142,25 +145,6 @@ class NBodySimulationsView(Ui_MainWindow, QObject):
     def set_duration(self, duration: float) -> None:
         _ = SignalBlocker(self.dsbDuration)
         self.dsbDuration.setValue(duration)
-
-    @staticmethod
-    def create_table_float_item(value: float) -> QTableWidgetItem:
-        return QTableWidgetItem(f"{value:.6f}")
-
-    def row_index_of_body(self, body_name: str) -> int:
-        return self.twBodyData.indexFromItem(QTableWidgetItem(body_name)).row()
-
-    def selected_body(self) -> str:
-        selected_index = self.selected_row_index()
-        if selected_index != -1:
-            return self.body_name_from_index(selected_index)
-        return None
-
-    def selected_row_index(self) -> int:
-        selection_model = self.twBodyData.selectionModel()
-        if selection_model.hasSelection():
-            return selection_model.currentIndex().row()
-        return -1
 
     def set_as_editing(self, editing: bool) -> None:
         self.pbEdit.setChecked(editing)
@@ -202,3 +186,19 @@ class NBodySimulationsView(Ui_MainWindow, QObject):
 
     def play_simulation(self) -> None:
         self.interactive_plot.play_animation()
+
+    @staticmethod
+    def _create_table_value(value: float) -> QTableWidgetItem:
+        return QTableWidgetItem(f"{value:.6f}")
+
+    def _get_table_value(self, row_index: int, column_index: int) -> float:
+        return float(self.twBodyData.item(row_index, column_index).text())
+
+    def _index_of_body(self, row_index: int) -> str:
+        return self.twBodyData.item(row_index, 0).text()
+
+    def _selected_row_index(self) -> int:
+        selection_model = self.twBodyData.selectionModel()
+        if selection_model.hasSelection():
+            return selection_model.currentIndex().row()
+        return -1
