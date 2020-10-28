9152bf4b2169f51a8cc4341c0af5f17ddaa48b01
Robert Applin <robert.applin@stfc.ac.uk>
2020-10-28 21:10:16 +0000

    Add comments to the DoubleSpinBoxAction.

--- python/qt/doublespinbox_action.py
+++ python/qt/doublespinbox_action.py
@@ -4,11 +4,12 @@ from PyQt5.QtWidgets import QHBoxLayout, QLabel, QDoubleSpinBox, QWidget, QWidge
 
 
 class DoubleSpinBoxAction(QWidgetAction):
+    """A class which creates a custom QMenu action made up of a QLabel and QDoubleSpinBox."""
 
     def __init__(self, label: str, default_value: float, minimum: float, maximum: float, suffix: str):
+        """Initializes the QLabel, QDoubleSpinBox, and layout of this custom action."""
         super(QWidgetAction, self).__init__(None)
 
-        self.label = QLabel(label)
         self.doubleSpinBox = QDoubleSpinBox(None)
         self.doubleSpinBox.setMinimum(minimum)
         self.doubleSpinBox.setMaximum(maximum)
@@ -16,7 +17,7 @@ class DoubleSpinBoxAction(QWidgetAction):
         self.doubleSpinBox.setSuffix(suffix)
 
         self.layout = QHBoxLayout()
-        self.layout.addWidget(self.label)
+        self.layout.addWidget(QLabel(label))
         self.layout.addWidget(self.doubleSpinBox)
 
         self.widget = QWidget()
