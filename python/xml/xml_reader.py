fbf1b65f671c4883182176cff512ef96b63555b5
Robert Applin <robert.applin@stfc.ac.uk>
2020-11-01 15:00:59 +0000

    Create an xml reader.

--- python/xml/xml_reader.py
+++ python/xml/xml_reader.py
@@ -1,20 +1,37 @@
 # Project Repository : https://github.com/robertapplin/N-Body-Simulations
 # Authored by Robert Applin, 2020
-import os
-
 from xml.dom import minidom
 
-from PyQt5.QtCore import QFile, QResource, QTextStream
+from PyQt5.QtCore import QFile, QTextStream
+
+INTERFACE_PROPERTY_FILE = ":/interface-properties.xml"
+USER_INTERFACE_PROPERTY_FILE = ":/user-interface-properties.xml"
 
 
-def print_data():
+def get_interface_property(property_name: str) -> str:
+    """Returns the specified property from the interface-properties.xml file."""
+    return get_xml_file_property(INTERFACE_PROPERTY_FILE, property_name)
 
-    file = QFile(":/user-interface-properties.xml")
+
+def get_user_interface_property(property_name: str) -> str:
+    """Returns the specified property from the user-interface-properties.xml file."""
+    return get_xml_file_property(USER_INTERFACE_PROPERTY_FILE, property_name)
+
+
+def get_xml_file_property(filename: str, item_name: str) -> str:
+    """Get an item with the specified name from an xml file."""
+    file = QFile(filename)
     if file.open(QFile.ReadOnly):
-        mydoc = minidom.parseString(QTextStream(file).readAll())
+        document_string = minidom.parseString(QTextStream(file).readAll())
+        return get_xml_property(document_string, filename, item_name)
 
-        items = mydoc.getElementsByTagName('item')
+    raise RuntimeError(f"Could not open the file '{filename}'.")
 
-        for elem in items:
-            if elem.attributes['name'].value == "item2":
-                print(elem.firstChild.data)
+
+def get_xml_property(document_string: str, filename: str, item_name: str) -> str:
+    """Get an item with the specified name from an xml document string."""
+    for element in document_string.getElementsByTagName('item'):
+        if element.attributes['name'].value == item_name:
+            return element.firstChild.data
+
+    raise ValueError(f"The item '{item_name}' does not exist in the file '{filename}'.")
