59916047c02e6146f9d3450de075a4f21458f575
Robert Applin <robert.applin@stfc.ac.uk>
2020-11-21 17:09:07 +0000

    Remove unwanted test.

--- tests/unit_tests/python/plotting/test_body_marker.py
+++ tests/unit_tests/python/plotting/test_body_marker.py
@@ -27,12 +27,6 @@ def test_that_set_position_will_set_the_position_of_the_body(dummy_plot):
     assert dummy_plot.body_markers["Sun"].mouse_drag_start(1.0, 1.0)
 
 
-def test_that_set_velocity_will_set_the_velocity_of_the_body(dummy_plot):
-    dummy_plot.body_markers["Sun"].set_position(1.0, 1.0)
-    dummy_plot.body_markers["Sun"].set_velocity(0.5, 0.5)
-    assert dummy_plot.body_markers["Sun"].mouse_drag_start(1.5, 1.5)
-
-
 def test_that_mouse_drag_start_will_return_true_when_the_mouse_position_is_above_the_marker(dummy_plot):
     assert dummy_plot.body_markers["Sun"].mouse_drag_start(0.0, 0.0)
     assert dummy_plot.body_markers["Sun"].get_override_cursor() == Qt.ClosedHandCursor
