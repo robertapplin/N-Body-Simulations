28b4064a6cfc21036dd42ea6e23fca53a9b680bb
Applin <robert.applin@stfc.ac.uk>
2023-03-31 10:06:38 +0100

    Remove library output directory arg from setup.py

--- setup.py
+++ setup.py
@@ -21,31 +21,19 @@ class CMakeBuild(build_ext):
 
     def build_extension(self, extension: CMakeExtension) -> None:
         """Generates and builds the project using CMake."""
-        extension_directory = self._get_extension_directory(extension)
-        pybind11_directory = pybind11.get_cmake_dir()
-
         cmake_args = [
             f"-DPYTHON_EXECUTABLE={sys.executable}",
-            f"-Dpybind11_DIR={pybind11_directory}"
+            f"-Dpybind11_DIR={pybind11.get_cmake_dir()}"
         ]
 
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
         if "win" in self.plat_name:
-            cmake_args += [f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE={extension_directory}", "-A", "x64"]
-        else:
-            cmake_args += [f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extension_directory}"]
+            cmake_args += ["-A", "x64"]
 
         subprocess.check_call(["cmake", extension.source_directory] + cmake_args, cwd=self.build_temp)
         subprocess.check_call(["cmake", "--build", ".", "--config", "Release"], cwd=self.build_temp)
-
-    def _get_extension_directory(self, extension: CMakeExtension) -> str:
-        """Generates and builds the project using CMake."""
-        extension_directory = os.path.abspath(os.path.dirname(self.get_ext_fullpath(extension.name)))
-        if not extension_directory.endswith(os.path.sep):
-            extension_directory += os.path.sep
-        return extension_directory
 
 
 setup(
