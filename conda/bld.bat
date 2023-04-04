7c5955e82b8e78989344f9f27b51bed599ccbbe2
Applin <robert.applin@stfc.ac.uk>
2023-04-04 19:13:08 +0100

    Remove pinning of gtest and gmock

--- conda/bld.bat
+++ conda/bld.bat
@@ -5,7 +5,7 @@ cmake ^
     -Dpybind11_DIR=%LIBRARY_PREFIX%/share/cmake/pybind11 ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_LIBDIR=%SP_DIR% ^
-    -A x64 ^
+    -G "Visual Studio 17 2022" -A x64 ^
     ..
 
 if errorlevel 1 exit 1
