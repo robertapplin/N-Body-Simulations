462acb2f1f1ae754173516becfbf095e9040ea56
Applin <robert.applin@stfc.ac.uk>
2023-04-06 18:31:24 +0100

    Use inheritance for cmake presets

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,16 +1,15 @@
 mkdir build && cd build
 
-cmake ^
+cmake --preset=vs-release ^
     -DPYTHON_EXECUTABLE="%PYTHON%" ^
     -Dpybind11_DIR=%LIBRARY_PREFIX%/share/cmake/pybind11 ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_LIBDIR=%SP_DIR% ^
-    -G "Visual Studio 17 2022" -A x64 ^
     ..
 
 if errorlevel 1 exit 1
-cmake --build . --config Release
-cmake --build . --config Release --target install
+cmake --build .
+cmake --build . --target install
 if errorlevel 1 exit 1
 
 cd ../
--- conda/build.sh
+++ conda/build.sh
@@ -3,12 +3,11 @@ set -ex
 
 mkdir build && cd build
 
-cmake \
+cmake --preset=ninja \
     -DPYTHON_EXECUTABLE=$PYTHON \
     -Dpybind11_DIR=$PREFIX/share/cmake/pybind11 \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=$SP_DIR \
-    -G Ninja \
     ../
 
 ninja
